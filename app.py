8afa4b4a8f8fdc22b75224d9d35961dd344235be
dangeriik <122404712+dangeriik@users.noreply.github.com>
2023-05-03 20:34:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 def main():
   print("ahoj,čau,čau")
-<<<<<<< HEAD
   print("Bum")
-=======
   print("ahoj")
-
->>>>>>> ad9e31b780b035f884e4529d32c9ebf42679b206
+  
 if __name__ == "__main__":
   main()
